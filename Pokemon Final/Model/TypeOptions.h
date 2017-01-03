3beae508b7667e2c8871e742fd504d16de1132cd
Sam <sam.sutcliffe2@gmail.com>
2017-01-03 15:32:40 +0000

    Made better
    Reduced attack types,
    Improved jumptable

--- Pokemon Final/Model/TypeOptions.h
+++ Pokemon Final/Model/TypeOptions.h
@@ -3,10 +3,8 @@
 enum TypeTypes
 {
 	Normal,
-	Ground,
 	Water,
-	Electric,
-	Grass
+	Electric
 };
 /*
 Explination on types
@@ -16,19 +14,12 @@ Some types beat others, this gives large bonus modifiers to the damage calculati
 50% ineffective
 ----Normal----
 Normal:All 
-----Ground----
-Effective:Electric
-Normal:Ground
-Ineffective:Grass
 ----Water----
 
 ----Electric----
 Effective:Water
 Normal:Electric
-Ineffective:Ground,Grass
-----Grass:----
-Effective:
-
+Ineffective:Ground
 */
 //Hacky solution to a lookup table that is static
 static float GetMultiplier(int AttackerType,int DefenseType)
@@ -36,77 +27,6 @@ static float GetMultiplier(int AttackerType,int DefenseType)
 	const float MultEffective = 2;
 	const float MultNormal = 1;
 	const float MultIneffective = 0.5;
-	switch (AttackerType)
-	{
-	(int)(TypeTypes::Normal):
-		switch (DefenseType)
-		{
-		TypeTypes::Normal:
-			return MultNormal;
-		Ground:
-			return MultNormal;
-		Water:
-			return MultNormal;
-		Electric:
-			return MultNormal;
-		Grass:
-			return MultNormal;
-		}
-	Ground:
-		switch (DefenseType)
-		{
-		Normal:
-			return MultNormal;
-		Ground:
-			return MultNormal;
-		Water:
-			return MultNormal;
-		Electric:
-			return MultEffective;
-		Grass:
-			return Ineffective;
-		}
-	Water:
-		switch (DefenseType)
-		{
-		Normal:
-			return MultNormal;
-		Ground:
-			return MultNormal;
-		Water:
-			return MultNormal;
-		Electric:
-			return MultIneffective;
-		Grass:
-			return MultIneffective;
-		}
-	Electric:
-		switch (DefenseType)
-		{
-		Normal:
-			return MultNormal;
-		Ground:
-			return MultIneffective;
-		Water:
-			return MultEffective;
-		Electric:
-			return MultIneffective;
-		Grass:
-			return MultNormal;
-		}
-	Grass:
-		switch (DefenseType)
-		{
-		Normal:
-			return MultNormal;
-		Ground:
-			return MultNormal;
-		Water:
-			return MultNormal;
-		Electric:
-			return MultNormal;
-		Grass:
-			return MultEffective;
-		}
-	}
+	const float AttackerDefenderValues = [[MultNormal,MultNormal,MultNormal],[MultNormal,MultNormal,MultIneffective],[MultNormal,MultEffective,MultNormal]];
+	return AttackerDefenderValues[AttackerType][DefenseType];
 }
