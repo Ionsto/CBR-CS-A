1c9c9c523acdb92ad8f7b04c6d3b4cef6454b002
Sam <sam.sutcliffe2@gmail.com>
2017-01-03 15:59:03 +0000

    CBRCase added

--- Pokemon Final/Pokemon Final/CBRCase.h
+++ Pokemon Final/Pokemon Final/CBRCase.h
@@ -2,7 +2,14 @@
 class CBRCase
 {
 public:
+	std::unique_ptr<CBREnviroment> StartEnviroment;
+	std::unique_ptr<CBREnviroment> EndEnviroment;
+	int Move;
+	//This is a function of how effective the case was at improving 
+	float UtiltityValue;
 	CBRCase();
 	~CBRCase();
+	void CalculateUtility();
+	
 };
 
