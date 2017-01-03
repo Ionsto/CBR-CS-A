fedd8887eb853f9e8b245094fc06f2eff49761eb
Sam <sam.sutcliffe2@gmail.com>
2017-01-03 15:59:11 +0000

    Implamentation of instance

--- Pokemon Final/Pokemon Final/CBRInstance.cpp
+++ Pokemon Final/Pokemon Final/CBRInstance.cpp
@@ -1,1 +1,32 @@
+#include "CBRInstance.h"
 
+CBRInstance::CBRInstance()
+{
+	
+}
+
+CBRInstance::~CBRInstance()
+{
+}
+/*
+-First find nearest case
+*/
+void CBRInstance::GetMove(CBREnviroment * inputenv)
+{
+	//
+	if(CaseBase.IsEmpty())
+	{
+		//Make a random move
+		
+	}
+	else
+	{
+		//Get nearest case
+		//Make a random choice of what to 
+	}
+}
+
+void CBRInstance::ResolveAnswer(CBREnviroment * finalenv)
+{
+	CurrentCase->
+}
