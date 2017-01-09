3efddb39ed92175086428eeb589541883101cf25
Sam <sam.sutcliffe2@gmail.com>
2017-01-09 09:56:26 +0000

    Add moves

--- Pokemon Final/Pokemon Final/CBREnviroment.h
+++ Pokemon Final/Pokemon Final/CBREnviroment.h
@@ -1,5 +1,16 @@
+struct MoveCase {
+	int AttackSpeed; 
+ 	int BaseDamage; 
+ 	int MaxUses; 
+ 	int CurrentUses; 
+ 	int AttackType; 
+}
 struct PokmonCase {
-	float Health;	
+	float Health;
+	int Type;
+	int Atttack;
+	int Defence;
+	MoveCase Moves[4];
 };
 
 class CBREnviroment{
