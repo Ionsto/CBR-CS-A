b8dd8290ea1c44234551a3da8f952ec82423649f
Sam <sam.sutcliffe2@gmail.com>
2017-01-09 10:03:49 +0000

    Add nearest neighbour

--- Pokemon Final/Pokemon Final/CBRCaseBase.h
+++ Pokemon Final/Pokemon Final/CBRCaseBase.h
@@ -1,6 +1,11 @@
-
+#include <vector>
+struct CBRCaseDistance{
+  float Distance;
+  CBRCase * Case;  
+}
 class CBRCaseBase{
 public:
   CBRCaseBase();
   ~CBRCaseBase();
+  std::vector<CBRCaseDistance> GetKNN(int K,float threshold);
 };
