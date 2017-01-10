26502ff53c9f6f28d7c2029009aebcf5f573503c
Sam <sam.sutcliffe2@gmail.com>
2017-01-10 15:58:08 +0000

    Added exploration constants

--- Pokemon Final/Pokemon Final/CBRCase.h
+++ Pokemon Final/Pokemon Final/CBRCase.h
@@ -7,9 +7,14 @@ public:
 	int Move;
 	//This is a function of how effective the case was at improving 
 	float UtiltityValue;
-	//Exploration vs exploitation is a 
-	int Exploitation;
-	int Exploration;
+	//Exploration count is the amount of tests that have taken place for other moves, but identical enviroments, increments on exploration
+	int ExplorationCount;
+	//This is a counter, it is reset every ExplorationMaxTests tests, and it increments slightly on a +utility, and increments greatly on a -utility
+	//This exploration value is used to pick randomly whether the AI will exploit, or explore 
+	float Exploration;
+	//This is the max amount of tests that may happen, before the exploration is reset
+	int ExplorationMaxTests;
+	int ExplorationTestsCount;
 	CBRCase();
 	~CBRCase();
 	void GenerateRandomMove();
