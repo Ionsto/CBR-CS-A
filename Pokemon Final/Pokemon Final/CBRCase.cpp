127073f31843044657152e41ab4d2ab8e228b75f
Sam <sam.sutcliffe2@gmail.com>
2017-01-12 11:08:24 +0000

    Updated

--- Pokemon Final/Pokemon Final/CBRCase.cpp
+++ Pokemon Final/Pokemon Final/CBRCase.cpp
@@ -14,6 +14,11 @@ void CBRCase::GenerateRandomMove()
 	this->Move = rand()%4;
 }
 
+bool CBRCase::Exploit()
+{
+	return (Exploration * (rand()%50)) > 25;	
+}
+
 void CBRCase::CalculateUtility()
 {
 	Utility = CalculateFitness(EndEnviroment) - CalculateFitness(StartEnviroment);
