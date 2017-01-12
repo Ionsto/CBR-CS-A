bc4c32451cd399f5ec1fd9c4482fc85921e1ed69
Sam <sam.sutcliffe2@gmail.com>
2017-01-12 10:45:16 +0000

    Added weights

--- Pokemon Final/Pokemon Final/CBRCaseBase.h
+++ Pokemon Final/Pokemon Final/CBRCaseBase.h
@@ -3,9 +3,28 @@ struct CBRCaseDistance{
   float Distance;
   CBRCase * Case;  
 }
+struct MoveWeight { 
+ 	int AttackSpeed;  
+ 	int BaseDamage;  
+ 	int MaxUses;  
+ 	int CurrentUses;  
+ 	int AttackType;  
+} 
+struct PokmonWeight { 
+  float Health; 
+  int Type; 
+	int Atttack; 
+ 	int Defence; 
+  MoveWeight Moves[4];
+}
+struct CaseWeights{
+  PokemonWeight PlayerPokemon;
+  PokemonWeight EnemyPokemon;
+}
 class CBRCaseBase{
 public:
   //Weights for the distance
+  CaseWeights DistanceWeight;
   CBRCaseBase();
   ~CBRCaseBase();
   std::vector<CBRCaseDistance> GetKNN(int K,float threshold);
