37389daf366af0f17dd39f67a16afef5f386fb5f
Sam <sam.sutcliffe2@gmail.com>
2017-01-13 12:41:02 +0000

    Added implementation

--- Pokemon Final/Pokemon Final/CBRCaseBase.cpp
+++ Pokemon Final/Pokemon Final/CBRCaseBase.cpp
@@ -6,4 +6,16 @@ CBRCaseBase::CBRCaseBase()
 
 CBRCaseBase::~CBRCaseBase()
 {
+} 
+//K-Nearest neighbor 
+std::vector<CBRCaseDistance> CBRCaseBase::GetKNN(int K,float threshold)
+{
+	std::vector<CBRCaseDistance> NearestCases = std::vector<CBRCaseDistance>();
+	
+	return NearestCases;
+}
+//returns weighted eucliden distance between two Cases 
+float CBRCaseBase::GetDistance(CBRCase * a, CBRCase * b)
+{
+  return 0;
 }
