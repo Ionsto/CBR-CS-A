b7a6a25a9dfafb16bfbc5c379da6b3e6e50d7e40
Sam <sam.sutcliffe2@gmail.com>
2017-01-17 15:39:06 +0000

    Commented

--- Pokemon Final/Pokemon Final/CBRInstance.h
+++ Pokemon Final/Pokemon Final/CBRInstance.h
@@ -1,7 +1,12 @@
+/*
+This is an instance of an AI, each AI will have different trained learned patterns or enviroment weights.
+*/
 class CBRInstance{
 public:
 	float ExplorationConstant;
 	float IdenticalThreshold;
 	CBRInstance();
 	~CBRInstance();
+	void CBRInstance::GetMove(std::unique_ptr<CBREnviroment> startenv);
+	void CBRInstance::ResolveAnswer(std::unique_ptr<CBREnviroment> finalenv);
 };			
